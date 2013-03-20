25201a294613a4abe75a6f3e152b7c828d052f35
Robby2012 <robby.upm@gmail.com>
2013-03-20 18:43:27 +0100

    Converted tabs into spaces

--- static/script.js
+++ static/script.js
@@ -7,7 +7,7 @@ wz.app.addScript( 3, 'common', function( win, params ){
     var cakeFree = $('.preferences-hdd-cake-free', win);
     var oldPassword = $('#old-password', win).children('input');
     var newPassword = $('#new-password', win).children('input');
-	var renewPassword = $('#renew-password', win).children('input');
+    var renewPassword = $('#renew-password', win).children('input');
     var saveData = $('#save-data', win);
     var usernameInput = $('#username', win).children('input');
     var emailInput = $('#email', win).children('input');
@@ -165,195 +165,195 @@ wz.app.addScript( 3, 'common', function( win, params ){
 
     })
         
-	.on('mousedown', '#cancel-password', function(){
-					
-		$( '.preferences-bottom-content.password' ).transition({opacity:0},250).css('display','none');
-		$( '.preferences-bottom-content.account' ).css('display','block').transition({opacity:1},250);
-		
-		$('input').val('');
-		usernameInput.val( username );
-		emailInput.val( mail );
-		$('.preferences-account-middle article, .preferences-password-middle article').find('i').removeClass();
-		saveData.add(savePassword).removeClass('active').addClass('disabled');
-		
-	})
-	
-	.on('mousedown', '#save-data', function(){
-		
-		if( usernameInput.val().match( ' ' ) !== null ){
-			alert( 'Your username cannot contain whitespaces' );
-		}else if( emailInput.val().match( ' ' ) !== null ){
-			alert( 'Your email cannot contain whitespaces' );
-		}else if( !usernameInput.val().length ){
-			alert( 'Your username cannot be empty' );
-		}else if( !emailInput.val().length ){
-			alert( 'Your email cannot be empty' );
-		}else if( usernameInput.val() !== username || emailInput.val() !== email ){
-		
-			wz.config( function( error, config ){
-				config.changeUsername( usernameInput.val(), function( error ){
+    .on('mousedown', '#cancel-password', function(){
+                    
+        $( '.preferences-bottom-content.password' ).transition({opacity:0},250).css('display','none');
+        $( '.preferences-bottom-content.account' ).css('display','block').transition({opacity:1},250);
+        
+        $('input').val('');
+        usernameInput.val( username );
+        emailInput.val( mail );
+        $('.preferences-account-middle article, .preferences-password-middle article').find('i').removeClass();
+        saveData.add(savePassword).removeClass('active').addClass('disabled');
+        
+    })
+    
+    .on('mousedown', '#save-data', function(){
+        
+        if( usernameInput.val().match( ' ' ) !== null ){
+            alert( 'Your username cannot contain whitespaces' );
+        }else if( emailInput.val().match( ' ' ) !== null ){
+            alert( 'Your email cannot contain whitespaces' );
+        }else if( !usernameInput.val().length ){
+            alert( 'Your username cannot be empty' );
+        }else if( !emailInput.val().length ){
+            alert( 'Your email cannot be empty' );
+        }else if( usernameInput.val() !== username || emailInput.val() !== email ){
+        
+            wz.config( function( error, config ){
+                config.changeUsername( usernameInput.val(), function( error ){
 
-					saveData.removeClass('active').addClass('disabled');
-					$('#username', win).find('i').removeClass();
-					$('#email', win).find('i').removeClass();
-					
-					if( error ){
-						alert( error );
-						usernameInput.val( username );
-						emailInput.val( mail );
-					}else{
-						data();
-						alert( 'Your data has been changed successfully' );
-					}
-					
-				});
-			});
-			
-		}
-		
-	})
-	
-	.on('mousedown', '#save-password', function(){
-		
-		if( newPassword.val() === renewPassword.val() && newPassword.val() && renewPassword.val() && oldPassword.val() ){
-			
-			wz.config( function( error, config ){
-				config.changePassword( oldPassword.val(), newPassword.val(), function( error ){
+                    saveData.removeClass('active').addClass('disabled');
+                    $('#username', win).find('i').removeClass();
+                    $('#email', win).find('i').removeClass();
+                    
+                    if( error ){
+                        alert( error );
+                        usernameInput.val( username );
+                        emailInput.val( mail );
+                    }else{
+                        data();
+                        alert( 'Your data has been changed successfully' );
+                    }
+                    
+                });
+            });
+            
+        }
+        
+    })
+    
+    .on('mousedown', '#save-password', function(){
+        
+        if( newPassword.val() === renewPassword.val() && newPassword.val() && renewPassword.val() && oldPassword.val() ){
+            
+            wz.config( function( error, config ){
+                config.changePassword( oldPassword.val(), newPassword.val(), function( error ){
 
-					savePassword.removeClass('active').addClass('disabled');
-					$('#renew-password', win).find('i').removeClass();
-					
-					if( error ){
-						oldPassword.val('');
-						oldPassword.focus();
-						alert( error );
-					}else{
-						oldPassword.val('');
-						newPassword.val('');
-						$('#renew-password', win).children('input').val('');
-						alert( 'Your password has been changed successfully' );
-					}
-					
-				});
-			});
-			
-		}
-		
-	})
-	
-	.on('mousedown', '#old-password figure i', function(){
-		
-		alert( 'Please send us a mail to weezeel@weezeel.com asking us to retrieve your password. Thank you.' );
-	
-	})
-	
-	.on('mousedown', '.preferences-account-middle article figure', function(){
-		$(this).siblings('input').focus();
-	})
-	
-	.on('mousedown', '#new-password figure, #renew-password figure', function(){
-		$(this).siblings('input').focus();
-	})
-	
-	.on('focus', '.preferences-account-middle article, .preferences-password-middle article', function(){
+                    savePassword.removeClass('active').addClass('disabled');
+                    $('#renew-password', win).find('i').removeClass();
+                    
+                    if( error ){
+                        oldPassword.val('');
+                        oldPassword.focus();
+                        alert( error );
+                    }else{
+                        oldPassword.val('');
+                        newPassword.val('');
+                        $('#renew-password', win).children('input').val('');
+                        alert( 'Your password has been changed successfully' );
+                    }
+                    
+                });
+            });
+            
+        }
+        
+    })
+    
+    .on('mousedown', '#old-password figure i', function(){
+        
+        alert( 'Please send us a mail to weezeel@weezeel.com asking us to retrieve your password. Thank you.' );
+    
+    })
+    
+    .on('mousedown', '.preferences-account-middle article figure', function(){
+        $(this).siblings('input').focus();
+    })
+    
+    .on('mousedown', '#new-password figure, #renew-password figure', function(){
+        $(this).siblings('input').focus();
+    })
+    
+    .on('focus', '.preferences-account-middle article, .preferences-password-middle article', function(){
 
-		$(this).addClass('active');
-		
-	})
-	
-	.on('blur', 'input', function(){
+        $(this).addClass('active');
+        
+    })
+    
+    .on('blur', 'input', function(){
 
-		$(this).parent().removeClass('active');
-		
-	})
-	
-	.on('keyup', '#username, #email', function(){
-		
-		clearInterval(idInterval);
-		idInterval = setTimeout( function(){
+        $(this).parent().removeClass('active');
+        
+    })
+    
+    .on('keyup', '#username, #email', function(){
+        
+        clearInterval(idInterval);
+        idInterval = setTimeout( function(){
 
-			var usernameLength = usernameInput.val().length;
-			var emailLength = emailInput.val().length;
-			
-			if( usernameLength || emailLength ){
-				
-				var userEqual = usernameInput.val() === username;
-				var mailEqual = emailInput.val() === mail;
-				
-				if( usernameLength && !userEqual && usernameInput.val().match( ' ' ) === null){
-					usernameInput.siblings('figure').children('i').addClass('process');
-				}else{
-					usernameInput.siblings('figure').children('i').removeClass('process');
-				}
-				
-				if( emailLength && !mailEqual && emailInput.val().match( ' ' ) === null){
-					emailInput.siblings('figure').children('i').addClass('process');
-				}else{
-					emailInput.siblings('figure').children('i').removeClass('process');
-				}
+            var usernameLength = usernameInput.val().length;
+            var emailLength = emailInput.val().length;
+            
+            if( usernameLength || emailLength ){
+                
+                var userEqual = usernameInput.val() === username;
+                var mailEqual = emailInput.val() === mail;
+                
+                if( usernameLength && !userEqual && usernameInput.val().match( ' ' ) === null){
+                    usernameInput.siblings('figure').children('i').addClass('process');
+                }else{
+                    usernameInput.siblings('figure').children('i').removeClass('process');
+                }
+                
+                if( emailLength && !mailEqual && emailInput.val().match( ' ' ) === null){
+                    emailInput.siblings('figure').children('i').addClass('process');
+                }else{
+                    emailInput.siblings('figure').children('i').removeClass('process');
+                }
 
-				if( ( !userEqual ||  !mailEqual ) && usernameLength && emailLength 
-					&& usernameInput.val().match( ' ' ) === null && emailInput.val().match( ' ' ) === null ){
-					saveData.removeClass('disabled').addClass('active');
-				}else{
-					saveData.removeClass('active').addClass('disabled');
-				}
-				
-			}else{
+                if( ( !userEqual ||  !mailEqual ) && usernameLength && emailLength 
+                    && usernameInput.val().match( ' ' ) === null && emailInput.val().match( ' ' ) === null ){
+                    saveData.removeClass('disabled').addClass('active');
+                }else{
+                    saveData.removeClass('active').addClass('disabled');
+                }
+                
+            }else{
 
-				usernameInput.siblings('figure').children('i').removeClass('process');
-				emailInput.siblings('figure').children('i').removeClass('process');
-				saveData.removeClass('active').addClass('disabled');
-				
-			}
+                usernameInput.siblings('figure').children('i').removeClass('process');
+                emailInput.siblings('figure').children('i').removeClass('process');
+                saveData.removeClass('active').addClass('disabled');
+                
+            }
 
-		}, 500);
+        }, 500);
 
-	})
+    })
 
-	.on('keyup', '#renew-password, #new-password, #old-password', function(){
+    .on('keyup', '#renew-password, #new-password, #old-password', function(){
 
-		if( $(this).attr('id') === 'new-password' || $(this).attr('id') === 'renew-password' ){
-			$('#renew-password').find('i').removeClass();
-		}
+        if( $(this).attr('id') === 'new-password' || $(this).attr('id') === 'renew-password' ){
+            $('#renew-password').find('i').removeClass();
+        }
 
-		clearInterval(idInterval);
-		idInterval = setTimeout( function(){
+        clearInterval(idInterval);
+        idInterval = setTimeout( function(){
 
-			var element = $('#renew-password');
-			var valNewPassword = $('#new-password').children('input').val();
-			var valreNewPassword = element.children('input').val();
+            var element = $('#renew-password');
+            var valNewPassword = $('#new-password').children('input').val();
+            var valreNewPassword = element.children('input').val();
 
-			if( valNewPassword.length && valreNewPassword.length ){
+            if( valNewPassword.length && valreNewPassword.length ){
 
-				if( valreNewPassword === valNewPassword ){
-					element.find('i').removeClass('error').addClass('tick');
-					if( $('#old-password').children('input').val().length ){
-						$('#save-password').removeClass('disabled').addClass('active');
-					}else{
-						$('#save-password').removeClass('active').addClass('disabled');
-					}
-				}else{
-					element.find('i').removeClass('tick').addClass('error');
-					$('#save-password').removeClass('active').addClass('disabled');
-				}
+                if( valreNewPassword === valNewPassword ){
+                    element.find('i').removeClass('error').addClass('tick');
+                    if( $('#old-password').children('input').val().length ){
+                        $('#save-password').removeClass('disabled').addClass('active');
+                    }else{
+                        $('#save-password').removeClass('active').addClass('disabled');
+                    }
+                }else{
+                    element.find('i').removeClass('tick').addClass('error');
+                    $('#save-password').removeClass('active').addClass('disabled');
+                }
 
-			}else{
-				element.find('i').removeClass();
-				$('#save-password').removeClass('active').addClass('disabled');
-			}
+            }else{
+                element.find('i').removeClass();
+                $('#save-password').removeClass('active').addClass('disabled');
+            }
 
-		}, 500);
+        }, 500);
 
-	})
-	
-	.key( 'enter', function(e){
-		if( $(e.target).is( '.preferences-account-middle input' ) ){
-			$('#save-data').mousedown();
-		}else if( $(e.target).is( '.preferences-password-middle input' ) ){
-			$('#save-password').mousedown();
-		}
-	});
+    })
+    
+    .key( 'enter', function(e){
+        if( $(e.target).is( '.preferences-account-middle input' ) ){
+            $('#save-data').mousedown();
+        }else if( $(e.target).is( '.preferences-password-middle input' ) ){
+            $('#save-password').mousedown();
+        }
+    });
 
     // Start App
     win
